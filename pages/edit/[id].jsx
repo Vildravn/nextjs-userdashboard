2539ff81e871203c20aa63d54914b1efdd9439ad
Vildravn <vildravn@gmail.com>
2023-11-24 11:37:00 +0100

    Task 3: Add user edit form

--- pages/edit/[id].jsx
+++ pages/edit/[id].jsx
@@ -3,10 +3,28 @@ import React from 'react'
 import PrimaryLayout from '@/layout/Primary'
 
 const EditUser = ({ user }) => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    const data = {
+      name: event.target.name.value,
+      username: event.target.username.value,
+      email: event.target.email.value,
+    }
+
+    console.log(JSON.stringify(data))
+  }
   return (
-    <pre>
-      {JSON.stringify(user, true, 2)}
-    </pre>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label><br />
+      <input type="text" id="name" name="name" defaultValue={`${user.name}`} /><br />
+      <label htmlFor="username">Username</label><br />
+      <input type="text" id="username" name="username" defaultValue={`${user.username}`} /><br />
+      <label htmlFor="email">E-Mail</label><br />
+      <input type="text" id="email" name="email" defaultValue={`${user.email}`} /><br />
+
+      <button type="submit">Submit</button>
+    </form>
   )
 }
 
